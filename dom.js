135ea5e5edcc4ec5d6fcb289edccc01bdd75eef2
nikhilmahar <nikhilmahar@iitg.ac.in>
2023-01-23 11:46:45 +0530

    add dom and html1

---
+++ dom.js
@@ -0,0 +1,10 @@
+console.log(document.type)
+console.log(document.getElementById)
+var headertitle=document.getElementById('header-title')
+// console.log()
+headertitle.style.borderBottom="solid 3px"
+var additem=document.getElementsByClassName('title')
+additem[0].style.color="green"
+additem[0].fontWeight="bold"
+console.log(additem)
+additem.style.color="#111"
