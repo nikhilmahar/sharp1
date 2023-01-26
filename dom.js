34abc7f03cedc0007953d7f5bb95d57a43f468b5
nikhilmahar <nikhilmahar@iitg.ac.in>
2023-01-26 16:07:30 +0530

    dom.js modified

--- dom.js
+++ dom.js
@@ -1,10 +1,17 @@
-console.log(document.type)
-console.log(document.getElementById)
-var headertitle=document.getElementById('header-title')
-// console.log()
-headertitle.style.borderBottom="solid 3px"
-var additem=document.getElementsByClassName('title')
-additem[0].style.color="green"
-additem[0].fontWeight="bold"
-console.log(additem)
-additem.style.color="#111"
+// console.log(document.type)
+// console.log(document.getElementById)
+// var headertitle=document.getElementById('header-title')
+// // console.log()
+// headertitle.style.borderBottom="solid 3px"
+// var additem=document.getElementsByClassName('title')
+// additem[0].style.color="green"
+// additem[0].fontWeight="bold"
+// console.log(additem)
+// additem.style.color="#111"
+
+var items=document.getElementsByClassName('list-group-item')
+console.log(items)
+items[2].style.backgroundColor="green"
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold"
+}
