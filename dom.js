5af3af3368f2813f963ce8393b0e9f1bb07d9e6b
nikhilmahar <nikhilmahar@iitg.ac.in>
2023-01-28 12:16:40 +0530

    changes made

--- dom.js
+++ dom.js
@@ -9,9 +9,18 @@
 // console.log(additem)
 // additem.style.color="#111"
 
-var items=document.getElementsByClassName('list-group-item')
-console.log(items)
-items[2].style.backgroundColor="green"
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold"
+// var items=document.getElementsByClassName('list-group-item')
+// console.log(items)
+// items[2].style.backgroundColor="green"
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold"
+// }
+
+var li=document.getElementsByTagName('li')
+console.log(li)
+li[2].style.backgroundColor="green"
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight="bold"
+    li[i].style.backgroundColor="pink"
+    
 }
